6591cf5683ecf50d3a3237e4652ff649ac9c8347
David Gofman <dgofman@gmail.com>
2022-08-25 18:44:19 -0700

    conif.fish alias

---
+++ config.fish
@@ -0,0 +1,13 @@
+source ~/kubectl-completion.fish
+alias cls=clear
+alias k='kubectl'
+alias kd='k describe'
+alias kaf='k apply --force -f'
+alias kc='k create --dry-run=client -o yaml'
+alias kcp='k run -o yaml --dry-run=client --restart=Never --image'
+alias kg='k get --show-labels'
+alias kgp='k get pods --show-labels'
+alias kcr='k set resources --local -o yaml -f'
+alias kce='k set env --local -o yaml -f'
+alias kcb='k run testbb --image=busybox --restart=Never --command sleep infinity'
+alias keb='k exec testbb -it -- /bin/sh'

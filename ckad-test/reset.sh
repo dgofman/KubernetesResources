a5aa4dafa11f24a65a42114cb409cece6a9ac46e
David Gofman <dgofman@gmail.com>
2022-08-28 13:27:27 -0700

    Update reset.sh
    update script

--- ckad-test/reset.sh
+++ ckad-test/reset.sh
@@ -41,6 +41,12 @@ echo 'Creating 2 ...'
 
 kubectl.exe apply -R -f .
 
+sudo rm -rf /opt/course
+
+sudo cp ../opt/course /opt
+
+sudo chown -R $(id -u):$(id -g) /opt/course
+
 # Backup https://www.geeksforgeeks.org/backing-up-a-docker-container/
 # docker commit -p c6069c3215c3 cluster1-worker1
 # docker save -o ../ckad-test.tar cluster1-worker1
